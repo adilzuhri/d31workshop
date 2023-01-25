013addf0f32f06d32b0045f07f52b8d0f0a11596
Adil Zuhri <106576506+adilzuhri@users.noreply.github.com>
2023-01-25 14:43:18 +0800

    ngStyle and ngClass

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -14,6 +14,30 @@ export class EmployeeComponent {
 
   visible = this.ShowContent(this.number1, this.number2);
   
+  IsVisible = true;
+  
+  color : any;
+
+  Employee : any [] = [];
+  
+  constructor(){
+    this.Employee = [
+      {
+        Name:'Ah Tan',
+        Age: '21',
+        Gender: 'M'
+      } , {
+        Name:'Ah Lim',
+        Age: '22',
+        Gender: 'M'
+      } , {
+        Name:'Ah Sim',
+        Age: '23',
+        Gender: 'F'
+      }
+    ]
+  }
+
   ShowContent(val1: number, val2: number) : boolean {
     if (val1 < val2) {
       return true;
@@ -22,14 +46,57 @@ export class EmployeeComponent {
     }
   }
 
-  IsVisible = true;
-  color : any;
-
   OnChange (arg : any) {
     this.IsVisible = arg;
   }
 
   SetColor(event: any) {
     this.color = event.target.value;
+  }
+
+  GetMoreEmployee() : void {
+    this.Employee = [
+      {
+        Name:'Ah Tan',
+        Age: '21',
+        Gender: 'M'
+      } , {
+        Name:'Ah Lim',
+        Age: '22',
+        Gender: 'M'
+      } , {
+        Name:'Ah Sim',
+        Age: '23',
+        Gender: 'F'
+      } ,  {
+        Name:'Ah Lan',
+        Age: '18',
+        Gender: 'M'
+      } , {
+        Name:'Ah Bam',
+        Age: '19',
+        Gender: 'M'
+      } , {
+        Name:'Ah Moi',
+        Age: '16',
+        Gender: 'F'
+      }
+    ]
+  }
+
+  CheckAge(Age:number) {
+    let val = Age < 18 ? 'Under 18' : 'Above 18';
+
+    switch(val) {
+      case 'Under 18':
+        return 'red';
+      case 'Above 18':
+        return 'green';   
+   }
+   return "";
+  }
+
+  TrackByEmployeeName (index: number, employee: any) : string { 
+    return employee.Name;
   }
 }
